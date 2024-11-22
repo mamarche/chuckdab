83b9067f9b3e7df6bfb1e969ba53b882bdad74f6
Marcello Marchetti <marcellomarchetti@hevolus.it>
2024-11-22 12:09:04 +0100

    add graphql relationship

--- ChuckKnowledge.App/Program.cs
+++ ChuckKnowledge.App/Program.cs
@@ -9,13 +9,16 @@ namespace ChuckKnowledge.App
         static async Task Main(string[] args)
         {
             var client = new ChuckClient(
-                "https://aca-wpc2024-chuck.nicerock-92961ea4.italynorth.azurecontainerapps.io/"
+                "https://aca-wpc2024-chuck.kindfield-7b68420d.italynorth.azurecontainerapps.io/"
             );
-            var fact = await client.GetItemAsync<Fact>(1);
-            Console.WriteLine(fact.FactDescription);
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            
+            int i=1;
+            do 
+            {
+                var fact = await client.GetItemAsync<Fact>(i);
+                Console.WriteLine(fact.FactDescription);    
+                i++;
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
